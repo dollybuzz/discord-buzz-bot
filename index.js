ef7100e4cfa5d2dda82e5be8b57e12a6487adc45
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2024-12-20 00:31:10 +0100

    correct user declaration

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const mysql = require('mysql2');
 //Create a database connection
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: process.env.DB_PORT,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
   });
