356ed4725f0f3a225f239b41b0bfad1ec2407329
dollybuzz <43527456+dollybuzz@users.noreply.github.com>
2025-01-01 23:23:03 +0100

    adjust query

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ try {
 
     // Retrieve the active question
     currentQuestion = await (async () => {
-        const [rows] = await connection.query('SELECT * FROM qotw_questions WHERE is_active = TRUE');
+        const [rows] = await connection.query('SELECT question FROM qotw_questions WHERE is_active = TRUE');
         console.log('Row retrieved from DB:', rows[0]);
         return rows[0];
     })();
